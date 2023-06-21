bcd1100a9bb2bd45b2e53f7814c3c8514794a1a4
sen <kseniya2495@yandex.ru>
2023-06-21 17:14:33 +0300

    циклы

---
+++ sem_2/1.py
@@ -0,0 +1,14 @@
+''' цикл с параметром for 
+for i in range(5):
+    print('hello')
+
+цикл с условием while
+count = 0
+while count < 5:
+    print('hello')
+    count += 1 '''
+
+summa = 0
+for number in range(1, 101):
+    summa += number
+print(summa)
