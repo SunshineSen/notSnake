b968cff83b9f98632602e2264101d6710c2a631e
sen <kseniya2495@yandex.ru>
2023-06-22 12:30:14 +0300

    степень 2 в двух решениях

--- sem_2/s_014.py
+++ sem_2/s_014.py
@@ -1,8 +1,17 @@
 ''' Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.'''
-N = int (input('введите ограничение N: '))
+
+
+''' N = int (input('введите ограничение N: '))
 degree = 0
 result = 0
 while degree <= N:
     result = 2**degree
     print ('2 в сепени', degree, 'равно', result)
     degree += 1
+'''
+N = int (input('введите ограничение N: '))
+degree = 0
+result = 0
+for degree in range(N+1):
+    result = 2**degree
+    print ('2 в сепени', degree, 'равно', result)
