2868a32ba15ca93e8ccf08e0c616bd045126cbef
sen <kseniya2495@yandex.ru>
2023-06-28 15:26:40 +0300

    теория

---
+++ sem_3/1.py
@@ -0,0 +1,21 @@
+#my_list = [1, -5.4, 'hello', [1, 2, 3]]
+#print (my_list)
+#for elem in my_list:
+    #print(elem)
+
+#for i in range(4):
+    #print(my_list[i])
+
+''' lisst = [1, 5, 7, 6]
+maxim = 0
+for i in range(4):
+    if lisst[i] > maxim:
+        maxim = lisst[i]
+print(maxim) 
+print(max(lisst))'''
+#listt = [0]  * 3 #заполнение тремя нулями
+
+#срез
+mylist = [1, 2, 3, 4, 5, 6, 7, 8]
+mylist2 = mylist[0::2] # удобно через срез копировать список [:]
+print(mylist2) 
